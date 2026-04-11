8d4726c92dbd7cf8c46eb117a8a0c017e5b7fed6
dimangty <dimangty@mail.ru>
2026-04-11 14:53:09 +0700

    Fix

--- shared/news/src/commonTest/kotlin/com/example/kursovikkmp/feature/gpt/feature/news/details/NewsDetailsViewModelCommonTest.kt
+++ shared/news/src/commonTest/kotlin/com/example/kursovikkmp/feature/gpt/feature/news/details/NewsDetailsViewModelCommonTest.kt
@@ -19,6 +19,7 @@ class NewsDetailsViewModelCommonTest : BaseViewModelCommonTest() {
     private val favoritesRepository =
         com.example.kursovikkmp.gpt.feature.news.details.FakeDetailsFavoritesRepository()
 
+
     @Test
     fun initScreenData_loadsArticleState() = runTest {
         // Given
