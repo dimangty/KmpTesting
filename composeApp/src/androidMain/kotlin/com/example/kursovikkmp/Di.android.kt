a7fedc58442effb6738117d58e87a22f0c4bea2f
Dmitry Bykov <dmitriy.bykov@elementpay.io>
2026-06-20 17:48:32 +0700

    Fix

--- composeApp/src/androidMain/kotlin/com/example/kursovikkmp/Di.android.kt
+++ composeApp/src/androidMain/kotlin/com/example/kursovikkmp/Di.android.kt
@@ -1,6 +1,10 @@
 package com.example.kursovikkmp
 
+import com.example.kursovikkmp.common.device.DeviceService
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
-actual val platformModule: Module = module {}
+internal actual val platformModule: Module = module {
+    singleOf(::DeviceService)
+}
--- composeApp/src/iosMain/kotlin/com/example/kursovikkmp/Di.ios.kt
+++ composeApp/src/iosMain/kotlin/com/example/kursovikkmp/Di.ios.kt
@@ -1,8 +1,10 @@
 package com.example.kursovikkmp
 
+import com.example.kursovikkmp.common.device.DeviceService
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 internal actual val platformModule: Module = module {
-    // iOS-specific dependencies
+    singleOf(::DeviceService)
 }
